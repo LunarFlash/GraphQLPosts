41601f115600e00fac5178d5f30d691fca617758
Terry Wang <terry.wang@accenture.com>
2018-07-12 23:18:46 -0400

    add api

--- PostsApp/Constants.swift
+++ PostsApp/Constants.swift
@@ -4,11 +4,11 @@
 //
 
 import Foundation
-//import AWSCore
-//
+import AWSCore
+
 //let CognitoIdentityPoolId = ""
-//let CognitoIdentityRegion: AWSRegionType = .USEast2
-//let AppSyncRegion: AWSRegionType = .USWest2
-//let AppSyncEndpointURL: URL = URL(string: "https://yourendpoint.us-west-2.amazonaws.com/graphql")!
-//let database_name = "appsync-local-db"
+//let CognitoIdentityRegion: AWSRegionType = .USEast1
+let AppSyncRegion: AWSRegionType = .USEast1
+let AppSyncEndpointURL: URL = URL(string: "https://7kytgep6mzf7teopygslxlkxlu.appsync-api.us-east-1.amazonaws.com/graphql")!
+let database_name = "appsync-local-db"
 
